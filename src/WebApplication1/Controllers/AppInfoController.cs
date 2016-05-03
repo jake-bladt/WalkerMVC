bc6c39ab531440d8f2f9a1e074f7885010387bb6
Jake <jake@jakebladt.com>
2016-05-03 19:59:50 -0400

    +simple controller

---
+++ src/WebApplication1/Controllers/AppInfoController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class AppInfoController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { ApplicationName = "Walker" });
+        }
+    }
+}
