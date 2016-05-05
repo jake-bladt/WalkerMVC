d6ee61bbc08035d12ffab367e1081506a21d82ac
Jake <jake@jakebladt.com>
2016-05-04 21:59:41 -0400

    config ninject

---
+++ src/Walker.UI/App_Start/NinjectModules.cs
@@ -0,0 +1,26 @@
+﻿using Ninject.Modules;
+
+namespace Walker.UI.Ninject
+{
+    public class NinjectModules
+    {
+        //Return Lists of Modules in the Application
+        public static NinjectModule[] Modules
+        {
+            get
+            {
+                //Return Modules to use for DI
+                return new[] { new MainModule() };
+            }
+        }
+
+        //Main Module For Application. 
+        public class MainModule : NinjectModule
+        {
+            public override void Load()
+            {
+                //TODO: Bind to Concrete Types Here
+            }
+        }
+    }
+}
